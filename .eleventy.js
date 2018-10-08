c66bce90ea1881e3b0e201672dbb77403c379ee9
Hunter Miller <svarty1@gmail.com>
2018-10-07 22:32:26 -0500

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -53,7 +53,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin");
 
   /* Markdown Plugins */
