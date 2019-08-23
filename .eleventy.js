499aefb785c599d4b77a5077a768849de81c7090
Hunter Miller <svarty1@gmail.com>
2019-08-22 21:31:41 -0500

    maybe right this time

--- .eleventy.js
+++ .eleventy.js
@@ -53,6 +53,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
+  eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("_includes/assets/js");
   eleventyConfig.addPassthroughCopy("admin");
